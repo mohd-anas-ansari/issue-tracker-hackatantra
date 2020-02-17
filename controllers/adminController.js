00599df18acb6928f7c736a6387b203d925cb86e
Kabir <kabir4691@gmail.com>
2020-02-17 23:40:08 +0530

    Generate JWT token only on user/admin login/register. Regenerate username from token on other APIs and pass it to request body.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,32 +2,43 @@ var Admin = require('../models/admin');
 const authController = require('./authController');
 
 module.exports = {
-  adminRegister: (req, res, next) => {
-    console.log(req.body);
-    Admin.create(req.body, (err, admin) => {
-      if (err) return next(err);
-      const accessToken = authController.generateToken(req);
-      res.status(200).json({
-        username: admin.username,
-        email: admin.email,
-        password: admin.password,
-        accessToken: accessToken,
+  adminRegister: (req, res) => {
+    console.log("Trying to register admin", req.body);
+    authController.generateToken(req)
+      .then(token => {
+        Admin.create(req.body, (err, admin) => {
+          if (err) return res.sendStatus(400);
+          res.status(200).json({
+            username: admin.username,
+            email: admin.email,
+            accessToken: token,
+          });
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
       });
-    });
   },
-  adminLogin: function(req, res, next) {
-    const EMAIL = req.body.email;
-    const PASSWORD = req.body.password;
-    console.log(EMAIL, PASSWORD, 'Inside LogIn');
-    Admin.findOne({ email: EMAIL }, (err, adminData) => {
-      if (err) return err;
-      const accessToken = authController.generateToken(req);
-      res.status(200).json({
-        username: admin.username,
-        email: admin.email,
-        password: admin.password,
-        accessToken: accessToken,
+
+  adminLogin: function(req, res) {
+    console.log("Trying to login admin", req.body);
+    authController.generateToken(req)
+    .then(token => {
+      const username = req.body.username;
+      const password = req.body.password;
+      Admin.findOne({ username, password }, (err, admin) => {
+        if (err) return res.sendStatus(400);
+        res.status(200).json({
+          username: admin.username,
+          email: admin.email,
+          accessToken: token,
+        });
       });
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
     });
   },
 };
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,25 @@ var jwt = require('jsonwebtoken');
 
 module.exports = {
 
-	generateToken: req => jwt.sign(req.body.username, process.env.ACCESS_TOKEN_SECRET),
+	generateToken: req => {
+		return new Promise((resolve, reject) => {
+			if (!req.body.username) {
+				return reject(new Error("Username required"));
+			}
+			jwt.sign(req.body.username, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+				if (err) return reject(err);
+				resolve(token);
+			});
+		})
+	},
 
 	verifyToken: (req, res, next) => {
-		const username = req.body.username;
-	  	const authHeader = req.headers['authorization'];
+	  	const authHeader = req.headers.authorization;
 	  	const token = authHeader && authHeader.split(" ")[1];
 	  	if (token == null) return res.sendStatus(401);
-	  	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
-	    	if (err || username !== result) return res.sendStatus(403);
+	  	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
+	    	if (err) return res.sendStatus(403);
+	    	req.body.username = username;
 	    	next();
 	  	});
 	}
